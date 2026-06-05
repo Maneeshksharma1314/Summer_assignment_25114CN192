e39f7876138d62d6731b04901f5660d5d7ae755e
Maneeshksharma1314 <25114cn192@glbitm.ac.in>
2026-06-05 23:01:28 +0530

    Create q3.c

---
+++ DAY5/q3.c
@@ -0,0 +1,19 @@
+// Program to print factors of a number
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
