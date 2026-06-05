3740162f91cb7cd47dc43a4e2e1eea2d2d478c07
Maneeshksharma1314 <25114cn192@glbitm.ac.in>
2026-06-05 22:50:35 +0530

    Create q2.c

---
+++ DAY2/q2.c
@@ -0,0 +1,19 @@
+// To reverse a number
+#include <stdio.h>
+
+int main() {
+    int num, rem, rev = 0;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num /= 10;
+    }
+
+    printf("The reverse of the number is: %d\n", rev);
+
+    return 0;
+}
