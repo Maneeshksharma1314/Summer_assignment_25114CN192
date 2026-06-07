958259934dab93d63bd048ed361f0dbda9ece7b7
Maneeshksharma1314 <25114cn192@glbitm.ac.in>
2026-06-07 21:26:17 +0530

    Create q2.c

---
+++ DAY7/q2.c
@@ -0,0 +1,26 @@
+// Program to print Fibonacci series using recursion
+#include <stdio.h>
+
+int fibo(int n) {
+    if (n == 1) {
+        return 0;
+    } else if (n == 2) {
+        return 1;
+    } else {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter the Number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i++) {
+        int x = fibo(i);
+        printf("%d ", x);
+    }
+
+    return 0;
+}
